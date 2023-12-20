6ec3d171400d026290feb3b350c745f7cdb9e577
totsss <axetra@hotmail.com>
2023-12-20 21:06:08 +0000

    light update on flight

--- model/flight_model.py
+++ model/flight_model.py
@@ -1,4 +1,8 @@
 class Flight:
-    def __init__(self,flight_id) -> None:
+    def __init__(self, flight_id, departure, destination, date, time, ready: bool) -> None:
         self.flight_id = flight_id
-        
+        self.departure = departure
+        self.destination = destination
+        self.date = date
+        self.time = time
+        self.ready = ready
